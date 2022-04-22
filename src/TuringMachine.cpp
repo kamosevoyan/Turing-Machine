8e3338431a7ea5f4f50a8e1ae67fdb9120fa33a2
kamosevoyan <sevoyankamo1998@gmail.com>
2022-04-22 20:56:00 +0400

    version conflict correction

--- src/TuringMachine.cpp
+++ src/TuringMachine.cpp
@@ -112,20 +112,12 @@ void TM::TuringMachine::setInput(const std::string& input) &
 {
 	if (!constructed)
 	{
-<<<<<<< HEAD
 		throw std::string("Error: The machine has not been constructed yet.\n");
-=======
-		throw "Error: The machine has not been constructed yet.\n";
->>>>>>> 42691bc6691f92c1b61abd99af133a1279b885a2
 	}
 
 	if (input.length() == 0)
 	{
-<<<<<<< HEAD
 		throw std::string("Error: Null input\n");
-=======
-		throw "Error: Null input\n";
->>>>>>> 42691bc6691f92c1b61abd99af133a1279b885a2
 	}
 	
 	this->tape.set(input, this->emptySymbol);
@@ -185,19 +177,11 @@ void TM::TuringMachine::setInput(const std::string& input) &
 		{
 			if ( (this->alphabetSymbols.find(read) != std::string::npos) && (std::find(this->stateSymbols.begin(), this->stateSymbols.end(), state) != this->stateSymbols.end()) )
 			{
-<<<<<<< HEAD
 				throw std::string("Error: Unexpected input: read " + std::string(read, 1) + " in state " + state + "\n");
 			}
 			else
 			{
 				throw std::string("Error: Unknown input value " + std::string(read, 1) + "\n");
-=======
-				throw "Error: Unexpected input: read " + std::string(read, 1) + " in state " + state + "\n";				
-			}
-			else
-			{
-				throw "Error: Unknown input value " + std::string(read, 1) + "\n";
->>>>>>> 42691bc6691f92c1b61abd99af133a1279b885a2
 			}
 		}
 		
@@ -282,11 +266,7 @@ void TM::TuringMachine::setConfiguration(const std::stringstream& stream) &
 		}
 		else
 		{
-<<<<<<< HEAD
 			throw std::string("Error: Input/output alphabet value " + token + " is already given\n");
-=======
-			throw "Error: Input/output alphabet value " + token + " is already given\n";
->>>>>>> 42691bc6691f92c1b61abd99af133a1279b885a2
 		}
 	}
 	
@@ -300,11 +280,7 @@ void TM::TuringMachine::setConfiguration(const std::stringstream& stream) &
 		
 		else
 		{
-<<<<<<< HEAD
 			throw std::string("Error: State " + token + " is already given\n");
-=======
-			throw "Error: State " + token + " is already given\n";
->>>>>>> 42691bc6691f92c1b61abd99af133a1279b885a2
 		}
 	}
 	
@@ -342,11 +318,7 @@ void TM::TuringMachine::setConfiguration(const std::stringstream& stream) &
 			
 			if (data.tellg() == std::string::npos)
 			{
-<<<<<<< HEAD
 				throw  std::string("Error: Configuration values for state " + stateSymbols[row] + " and below are not given\n");
-=======
-				throw  "Error: Configuration values for state " + stateSymbols[row] + " and below are not given\n";				
->>>>>>> 42691bc6691f92c1b61abd99af133a1279b885a2
 			}
 		}
 			
@@ -358,11 +330,7 @@ void TM::TuringMachine::setConfiguration(const std::stringstream& stream) &
 		{
 			if (lmDtNy.tellg() == std::string::npos)
 			{
-<<<<<<< HEAD
 				throw std::string("Error: Less values than expected in " + std::to_string(row) +  " row " + std::to_string(column) + " column\n");
-=======
-				throw "Error: Less values than expected in " + std::to_string(row) +  " row " + std::to_string(column) + " column\n";
->>>>>>> 42691bc6691f92c1b61abd99af133a1279b885a2
 			}
 
 			std::getline(lmDtNy, token, '|');
@@ -384,29 +352,17 @@ void TM::TuringMachine::setConfiguration(const std::stringstream& stream) &
 						
 			if(std::find(this->alphabetSymbols.begin(), this->alphabetSymbols.end(), ltoken) == this->alphabetSymbols.end())
 			{
-<<<<<<< HEAD
 				throw std::string("Error: Unknows output value " + std::string(ltoken,1) + " in " + std::to_string(row) + " row " + std::to_string(column) + " column \n");
-=======
-				throw "Error: Unknows output value " + std::string(ltoken,1) + " in " + std::to_string(row) + " row " + std::to_string(column) + " column \n";
->>>>>>> 42691bc6691f92c1b61abd99af133a1279b885a2
 			}
 
 			if(std::find(this->stateSymbols.begin(), this->stateSymbols.end(), rtoken) == this->stateSymbols.end())
 			{
-<<<<<<< HEAD
 				throw std::string("Error: Unknown state value " + std::string(rtoken,1) + " in " + std::to_string(row) + " row " + std::to_string(column) + " column\n");
-=======
-				throw "Error: Unknown state value " + std::string(rtoken,1) + " in " + std::to_string(row) + " row " + std::to_string(column) + " column\n";
->>>>>>> 42691bc6691f92c1b61abd99af133a1279b885a2
 			}
 			
 			if(std::find(TuringMachine::dir.begin(), TuringMachine::dir.end(), dirtoken) == TuringMachine::dir.end())
 			{
-<<<<<<< HEAD
 				throw std::string("Error: Unknown direction value " +  std::string(dirtoken,1) + " in " + std::to_string(row) + " row " + std::to_string(column) + " column\n");
-=======
-				throw "Error: Unknown direction value " +  std::string(dirtoken,1) + " in " + std::to_string(row) + " row " + std::to_string(column) + " column\n";
->>>>>>> 42691bc6691f92c1b61abd99af133a1279b885a2
 			}
 			
 			this->lambda[std::make_pair(this->stateSymbols[row], this->alphabetSymbols[column])] = rtoken;
@@ -416,11 +372,7 @@ void TM::TuringMachine::setConfiguration(const std::stringstream& stream) &
 		
 		if(lmDtNy.tellg() != std::string::npos)
 		{
-<<<<<<< HEAD
 			throw std::string("Error: More values in " + std::to_string(row) + " row than expected\n");
-=======
-			throw "Error: More values in " + std::to_string(row) + " row than expected\n";
->>>>>>> 42691bc6691f92c1b61abd99af133a1279b885a2
 		}
 	}
 	
@@ -435,11 +387,7 @@ void TM::TuringMachine::setConfiguration(const std::stringstream& stream) &
 		
 		if(data.tellg() == std::string::npos)
 		{
-<<<<<<< HEAD
 			throw std::string("Error: Initial state value is not given\n");
-=======
-			throw "Error: Initial state value is not given\n";
->>>>>>> 42691bc6691f92c1b61abd99af133a1279b885a2
 		}
 	}
 	
@@ -447,11 +395,7 @@ void TM::TuringMachine::setConfiguration(const std::stringstream& stream) &
 	
 	if (std::find(this->stateSymbols.begin(), this->stateSymbols.end(), token) == this->stateSymbols.end())
 	{
-<<<<<<< HEAD
 		throw std::string("Error: Unknown initial state value " + token + "\n");
-=======
-		throw "Error: Unknown initial state value " + token + "\n";
->>>>>>> 42691bc6691f92c1b61abd99af133a1279b885a2
 	}
 	
 	this->initalState = token;
@@ -467,11 +411,7 @@ void TM::TuringMachine::setConfiguration(const std::stringstream& stream) &
 		
 		if(data.tellg() == std::string::npos)
 		{
-<<<<<<< HEAD
 			throw std::string("Error: Halt state value is not given\n");
-=======
-			throw "Error: Halt state value is not given\n";			
->>>>>>> 42691bc6691f92c1b61abd99af133a1279b885a2
 		}
 	}
 	
@@ -480,11 +420,7 @@ void TM::TuringMachine::setConfiguration(const std::stringstream& stream) &
 	
 	if (std::find(this->stateSymbols.begin(), this->stateSymbols.end(), token) == this->stateSymbols.end())
 	{
-<<<<<<< HEAD
 		throw std::string("Error: Unknown halt state value " + token + "\n");
-=======
-		throw "Error: Unknown halt state value " + token + "\n";
->>>>>>> 42691bc6691f92c1b61abd99af133a1279b885a2
 	}
 	
 	this->haltState = token;
@@ -501,11 +437,7 @@ void TM::TuringMachine::setConfiguration(const std::stringstream& stream) &
 		
 		if(data.tellg() == std::string::npos)
 		{
-<<<<<<< HEAD
 			throw std::string("Error: Empty symbol value is not given\n");
-=======
-			throw "Error: Empty symbol value is not given\n";
->>>>>>> 42691bc6691f92c1b61abd99af133a1279b885a2
 		}
 	}
 	
@@ -514,11 +446,7 @@ void TM::TuringMachine::setConfiguration(const std::stringstream& stream) &
 	
 	if (this->alphabetSymbols.find(ctoken) == std::string::npos)			
 	{
-<<<<<<< HEAD
 		throw std::string("Error: Enknown empty symbol state value " + token + "\n");
-=======
-		throw "Error: Enknown empty symbol state value " + token + "\n";
->>>>>>> 42691bc6691f92c1b61abd99af133a1279b885a2
 	}
 	
 	this->emptySymbol = ctoken;
